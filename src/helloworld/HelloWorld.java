a169ad3511a4c438d17e6500ea43e719da2d23b0
lunor7649 <lunor7649@7HJL6Y1.Student.UGDSB.ED>
2018-02-28 11:21:23 -0500

    This is my first commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+HelloWorld.java
+Created by: Lucas N-H
+Created on: February 23, 2018
+Last edited on: February 28, 2018
+*/
+package helloworld;
+
+public class HelloWorld {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    } 
+}
